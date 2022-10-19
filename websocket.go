f2914c9ad282c9c6789ea0b7dcc0a7463c9f2344
Beloin Sena <beloin.rodrigues@gmail.com>
2022-10-19 19:20:19 -0300

    Added documentation

--- websocket.go
+++ websocket.go
@@ -12,6 +12,13 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+// SEE:
+// https://github.com/gorilla/websocket
+// https://pkg.go.dev/github.com/gorilla/websocket
+// https://github.com/gorilla/websocket/blob/af47554f343b4675b30172ac301638d350db34a5/examples/chat/client.go#L82
+// https://github.com/gorilla/websocket/blob/af47554f343b4675b30172ac301638d350db34a5/examples/chat/hub.go#L9
+
+// EXAMPLE: https://javascript.info/websocket#:~:text=To%20open%20a%20websocket%20connection,It's%20like%20HTTPS%20for%20websockets.
 func main() {
 }
 
